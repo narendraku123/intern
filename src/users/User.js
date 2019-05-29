05151975d94a054dd06431328ddcf25ffbefbae1
NarendraKumar <jackjin987@gmail.com>
2019-05-29 00:04:35 -0500

    my first Commit

---
+++ src/users/User.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Person from '../Person/Person';
+const User = (props) => {
+return (<div>
+      Name : {props.children}  Age : {props.age}
+      console.log('user');
+      <Person name="Akhil" Age = "23">  </Person>
+      <Person />
+   </div>)
+
+}
+
+export default User;
